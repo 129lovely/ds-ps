5a5f03837e9740626a30b66b1010fe41935b7aee
129lovely <129lovely@gmail.com>
2021-03-11 21:35:44 +0900

    update linked_lists.py

--- ds/linked_lists.py
+++ ds/linked_lists.py
@@ -12,7 +12,6 @@ class LinkedList:
         self.head.next = self.tail
 
     # get a node by a particular index
-    # TODO
     def getAt(self, pos):
         if pos < 1 or pos > self.nodeCount:
             return None
@@ -32,52 +31,56 @@ class LinkedList:
             array.append(curr.data)
         return array
 
+    # insert new node after a particular node
+    def insertAfter(self, prev, newNode):
+        if prev.next is None:
+            self.tail = newNode
+
+        newNode.next = prev.next
+        prev.next = newNode
+
+        self.nodeCount += 1
+        return True
+
     # insert new node into a particular index
     def insertAt(self, pos, newNode):
         if pos < 1 or (self.nodeCount + 1) < pos:
             return False
 
-        if pos == 1:
-            newNode.next = self.head
-            self.head = newNode
+        if pos != 1 and pos == (self.nodeCount + 1):
+            prev = self.tail
         else:
-            if pos == (self.nodeCount + 1):
-                prev = self.tail
+            prev = self.getAt(pos - 1)
+
+        return self.insertAfter(prev, newNode)
+
+    # delete a node after a particular node
+    def popAfter(self, prev):
+        if prev.next is None:
+            return None
+
+        curr = prev.next
+        if curr.next is None:
+            if self.nodeCount == 1:
+                self.tail = None
             else:
-                prev = self.getAt(pos - 1)
-            newNode.next = prev.next
-            prev.next = newNode
+                self.tail = prev
+        prev.next = curr.next
 
-        if pos == (self.nodeCount + 1):
-            self.tail = newNode
-
-        self.nodeCount += 1
-        return True
+        self.nodeCount -= 1
+        return curr.data
 
     # delete a node by a particular index
     def popAt(self, pos):
         if pos < 1 or self.nodeCount < pos:
             raise IndexError
 
-        curr = None
         prev = self.getAt(pos - 1)
-        if prev is not None:
-            curr = prev.next
-            prev.next = curr.next
-        else:
-            curr = self.head
-            self.head = curr.next
-
-        if pos == self.nodeCount:
-            self.tail = prev
-
-        self.nodeCount -= 1
-
-        return curr.data
+        return self.popAfter(prev)
 
     # concatenate two linked lists
     def concat(self, newLinkedList):
-        self.tail.next = newLinkedList.head
-        if self.tail:
+        self.tail.next = newLinkedList.head.next
+        if newLinkedList.tail is not None:
             self.tail = newLinkedList.tail
         self.nodeCount += newLinkedList.nodeCount
