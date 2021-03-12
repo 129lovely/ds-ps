aa395b7aecb8bd250be5a235a3be744ece1e342c
129lovely <129lovely@gmail.com>
2021-03-12 16:23:53 +0900

    update doubly_linked_lists.py

--- ds/doubly_linked_lists.py
+++ ds/doubly_linked_lists.py
@@ -32,14 +32,22 @@ class DoublyLinkedList:
         return array
 
     # get a node by a particular index
-    def getAt(self):
+    def getAt(self, pos):
         if pos < 0 or self.nodeCount < pos:
             return None
+
         i = 0
-        curr = self.head
-        while i < pos:
-            curr = curr.next
-            i += 1
+        if pos > self.nodeCount // 2:
+            curr = self.tail
+            while i < self.nodeCount - pos + 1:
+                curr = curr.prev
+                i += 1
+        else:
+            curr = self.head
+            while i < pos:
+                curr = curr.next
+                i += 1
+
         return curr
 
     # insert a node after a particular node
@@ -52,12 +60,53 @@ class DoublyLinkedList:
         self.nodeCount += 1
         return True
 
+    # insert a node before a particular node
+    def insertBefore(self, next, newNode):
+        prev = next.prev
+        newNode.prev = prev
+        newNode.next = next
+        prev.next = newNode
+        next.prev = newNode
+        self.nodeCount += 1
+        return True
+
     # insert a node into a particular index
     def insertAt(self, pos, newNode):
-        pass
+        if pos < 1 or pos > self.nodeCount + 1:
+            return False
+        prev = self.getAt(pos - 1)
+        return self.insertAfter(prev, newNode)
 
-    def popAfter(self):
-        pass
+    # delete a node after a particular node
+    def popAfter(self, prev):
+        curr = prev.next
+        next = curr.next
+        prev.next = next
+        next.prev = prev
+        self.nodeCount -= 1
+        return curr.data
 
-    def popAt(self):
-        pass
+    # delete a node before a particular node
+    def popBefore(self, next):
+        curr = next.prev
+        prev = curr.prev
+        prev.next = next
+        next.prev = prev
+        self.nodeCount -= 1
+        return curr.data
+
+    # delete a note by a particular index
+    def popAt(self, pos):
+        if pos < 1 or self.nodeCount < pos:
+            raise IndexError
+        prev = self.getAt(pos - 1)
+        return self.popAfter(prev)
+
+    # concatenate two linked lists
+    def concat(self, newDoublyLinkedList):
+        link_prev = self.tail.prev
+        link_next = newDoublyLinkedList.head.next
+        link_prev.next = link_next
+        link_next.prev = link_prev
+        self.tail = newDoublyLinkedList.tail
+        self.nodeCount += newDoublyLinkedList.nodeCount
